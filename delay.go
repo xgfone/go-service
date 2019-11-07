a0fa6fc89a7e9797399103174729d1095110112f
xgfone <xgfone@126.com>
2019-11-07 20:20:42 +0800

    add the delay

---
+++ delay.go
@@ -0,0 +1,46 @@
+// Copyright 2019 xgfone
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package service
+
+import "time"
+
+// Delay is used to get the next delay duration.
+type Delay func() time.Duration
+
+// NewFixedDelay returns a Delay that always returns the same delay duration.
+func NewFixedDelay(delay time.Duration) Delay {
+	return func() time.Duration { return delay }
+}
+
+// NewMultipleDelay returns a delay that will increase the
+func NewMultipleDelay(start, end time.Duration) Delay {
+	if start < 1 || end < 1 {
+		panic("MultipleDelay: the start or end duration must be an positive integer")
+	} else if start > end {
+		panic("MultipleDelay: the start must not be greater than the end")
+	}
+
+	next := start
+	return func() (n time.Duration) {
+		n = next
+		if next < end {
+			next += next
+			if next > end {
+				next = end
+			}
+		}
+		return
+	}
+}
