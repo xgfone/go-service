4fadf21000f3f02c57012467f3266c639bc715b7
xgfone <xgfone@126.com>
2020-09-11 21:56:02 +0800

    add the package doc

--- httputil/httputil.go
+++ httputil/httputil.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package httputil supplies the middleware and the wrapper of http.RoundTripper.
 package httputil
 
 import (
